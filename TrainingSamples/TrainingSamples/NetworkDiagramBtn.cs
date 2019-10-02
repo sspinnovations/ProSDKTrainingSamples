b3f9d14a79f22e2cbd6d3980d2defe2438dcaedf
Justin Rowsell <Rowsell64@gmail.com>
2019-10-02 04:46:38 +0000

    comments for the network diagram button

--- TrainingSamples/TrainingSamples/NetworkDiagramBtn.cs
+++ TrainingSamples/TrainingSamples/NetworkDiagramBtn.cs
@@ -26,46 +26,53 @@ namespace TrainingSamples
         {
             try
             {
-                string unLayerName = "Electric Utility Network";
-                Map activeMap = MapView.Active.Map;
-                Layer unLayer = await GetLayerByName(activeMap, unLayerName);
-                UtilityNetwork utilityNetwork = await GetUNByLayer(unLayer);
+                // Step 1 - Get a reference to the utility network in the current map
+                // string unLayerName = "Electric Utility Network";
+                // Map activeMap = MapView.Active.Map;
+                // Layer unLayer = await GetLayerByName(activeMap, unLayerName);
+                // UtilityNetwork utilityNetwork = await GetUNByLayer(unLayer);
 
-                DiagramLayer diagramLayer = await QueuedTask.Run(() =>
-                {
-                    using (DiagramManager diagramManager = utilityNetwork.GetDiagramManager())
-                    {
-                        // Get selected global ids on the desired layer
-                        List<string> layerNames = new List<string>
-                    {
-                        "Transformer",
-                        "Service Point",
-                        "Low Voltage"
-                    };
+                // Step 2 - Create a queued task to create the diagram on the MCT thread
+                // QueuedTask.Run(() =>
+                // {
+                    // Step 3 - Get a reference to the diagram manager class in the utility network
+                    // using (DiagramManager diagramManager = utilityNetwork.GetDiagramManager())
+                    // {
+                        // Step 4 - Create a list of layer names to include in the diagram
+                        // List<string> layerNames = new List<string>
+                        // {
+                        //     "Transformer",
+                        //     "Service Point",
+                        //     "Low Voltage"
+                        // };
 
-                        List<Guid> globalIds = GetSelectionGlobalIds(activeMap, layerNames);
+                        // Step 5 - get a list of global ids from the selected features in the list of layers from the 
+                        // previous step
+                        // List<Guid> globalIds = GetSelectionGlobalIds(activeMap, layerNames);
 
-                        DiagramTemplate diagramTemplate = diagramManager.GetDiagramTemplate("CollapseContainers");
-                        NetworkDiagram networkDiagram = diagramManager.CreateNetworkDiagram(diagramTemplate, globalIds);
+                        // Step 6 - Get a core template and create a network diagram using the globabl ids of the features found in the selection
+                        // DiagramTemplate diagramTemplate = diagramManager.GetDiagramTemplate("CollapseContainers");
+                        // NetworkDiagram networkDiagram = diagramManager.CreateNetworkDiagram(diagramTemplate, globalIds);
 
-                        SmartTreeDiagramLayoutParameters diagramLayoutParameters = new SmartTreeDiagramLayoutParameters();
-                        diagramLayoutParameters.Direction = SmartTreeDiagramLayoutParameters.TreeDirection.FromTopToBottom;
-                        networkDiagram.ApplyLayout(diagramLayoutParameters);
+                        // Step 7 - Apply a smart tree diagram layout to the network diagram
+                        // SmartTreeDiagramLayoutParameters diagramLayoutParameters = new SmartTreeDiagramLayoutParameters();
+                        // diagramLayoutParameters.Direction = SmartTreeDiagramLayoutParameters.TreeDirection.FromTopToBottom;
+                        // networkDiagram.ApplyLayout(diagramLayoutParameters);
 
-                        // Create the diagram map
-                        var newMap = MapFactory.Instance.CreateMap(networkDiagram.Name, MapType.NetworkDiagram, MapViewingMode.Map);
-                        if (newMap == null)
-                            return null;
+                        // Step 8 - Create the diagram map
+                        // var newMap = MapFactory.Instance.CreateMap(networkDiagram.Name, MapType.NetworkDiagram, MapViewingMode.Map);
+                        // if (newMap == null)
+                        //     return null;
 
-                        // Open the diagram map
-                        var mapPane = ArcGIS.Desktop.Core.ProApp.Panes.CreateMapPaneAsync(newMap, MapViewingMode.Map);
-                        if (mapPane == null)
-                            return null;
+                        // Step 9 - Open the diagram map
+                        // var mapPane = ArcGIS.Desktop.Core.ProApp.Panes.CreateMapPaneAsync(newMap, MapViewingMode.Map);
+                        // if (mapPane == null)
+                        //     return null;
 
-                        //Add the diagram to the map
-                        return newMap.AddDiagramLayer(networkDiagram);
-                    }
-                });
+                        // Step 10 - Add the diagram to the map
+                        // DiagramLayer diagramLayer = newMap.AddDiagramLayer(networkDiagram);
+                //     }
+                // }).Wait();
             }
             catch (Exception ex)
             {
